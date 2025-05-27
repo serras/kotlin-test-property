a4d2afc39a3afa707b15ebda3bf66c3a56e3e95e
Alejandro Serrano Mena <trupill@gmail.com>
2025-05-27 21:20:11 +0200

    Generator for sealed hierarchy

--- example/src/commonTest/kotlin/com/serranofp/kotlin/test/property/Example.kt
+++ example/src/commonTest/kotlin/com/serranofp/kotlin/test/property/Example.kt
@@ -3,12 +3,32 @@ package com.serranofp.kotlin.test.property
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-class Example {
+sealed interface Example {
+    data class Number(val number: Int): Example
+    data class Thing(val string: String): Example
+
+    companion object {
+        context(_: GeneratorContext)
+        val generator: Generator<Example>
+            get() = choice(
+                2 to ::Number.by(int()),
+                3 to ::Thing.by(string())
+            )
+    }
+}
+
+class ExampleTests {
     @Test @PropertyTest
-    fun example() = propertyTest {
+    fun example1() = propertyTest {
         val pair by pair(int(), int(1, 50))
         @Suppress("UNUSED_VARIABLE")
         val other by list(int() or string(), size = 3)
         assertTrue(pair.second > 0)
+    }
+
+    @Test @PropertyTest
+    fun example2() = propertyTest {
+        val example by Example.generator
+        assertTrue(example is Example.Number || example is Example.Thing)
     }
 }
